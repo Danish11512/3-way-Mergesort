48b109c23d55d5c8a677e2bed443541b8f93436b
Danish11512 <danish.faruqi1@gmail.com>
2019-02-20 00:01:40 -0500

    Version 5 Commit - Finally Working

--- Assignment 1/Test.cpp
+++ Assignment 1/Test.cpp
@@ -1,39 +1,27 @@
+// Danish Faruqi
+
 #include <iostream>
 #include <stdlib.h>
-
-
-
 void merge_sort(int *a, int *aux, int length) {
     int temp, i, j, k, cursor = 0;
-
     if (length > 1)// base case takes care of 2 and 1
     {
-        //CCHANAGE
-
         if (length == 2) // change format
         {
-            if (*a > *(a + 1)) {
-                temp = *a;
-                *a = *(a + 1);
-                *(a + 1) = temp;
+            if (a[0] > a[1]) {
+                temp = a[0];
+                a[0] = a[1];
+                a[1] = temp;
             }
             return;
         }
-
-
         merge_sort(a, aux, (length / 3));
         merge_sort(a + (length / 3), aux, (length) / 3);
         merge_sort(a + (2 * length / 3), aux, length - (2 * length / 3));
 
-
-        //length/3;
-
-        //length - (2*length/3);
-
-        //length - (length/3);
-
         // one where each one is exhausted
         // one where two are exhausted, so just move all of the last one
+
         for (cursor = 0, i = 0, j = length / 3, k = (2 * length / 3); cursor < length;) {
             // If no array has been exhausted
             if (i < (length / 3) && j < (2 * length / 3) && k < length) {   // i is the smallest
@@ -98,6 +86,10 @@ void merge_sort(int *a, int *aux, int length) {
         for (i = 0; i < length; i++) /* copying back */
             a[i] = aux[i];
     }
+
+
+
+
 }
 
 
@@ -105,21 +97,16 @@ void merge_sort(int *a, int *aux, int length) {
 
 int main ()
 {
-    int *r, *s; long i;
-    printf("Allocating Array. "); fflush(stdout);
-    r = (int *) malloc(3000000 * sizeof(int));
-    s = (int *) malloc(3000000 * sizeof(int));
-    printf("Filling array. "); fflush(stdout);
-    for( i = 0; i< 3000000; i++)
-        r[i] = 3 * ((137 * i)%3000000);
-    printf("Sorting. "); fflush(stdout);
-    merge_sort( r, s, 3000000 );
-    printf("Checking. \n"); fflush(stdout);
-    for(i=0; i< 3000000; i++)
-        if( r[i] != 3*i )
-        {  printf("Error at %d; is %d, should be %d\n", i, r[i], 3*i );
-            fflush(stdout); exit(-1);
-        }
-    printf("Passed Test.\n");
+    int a[9] = { 4, 6, 9, 2, 13, 26, 74, 67, 97};
+    // 2, 4, 6, 9, 13, 26, 74, 67, 97
+    int aux[9] = {};
+
+    mergesortthird(a, aux, 9);
+
+    for ( int i = 0; i<10; i++)
+    {
+        std::cout<<a[i]<< std::endl;
+    }
+
 
 }
